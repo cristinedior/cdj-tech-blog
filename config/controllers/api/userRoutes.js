435c4a0e8e81e55548a46dff6ac349bced15cd10
cristinedior <148567509+cristinedior@users.noreply.github.com>
2024-05-20 17:35:22 -0500

    added inital file structure and code

---
+++ config/controllers/api/userRoutes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { User } = require('../models');
+
+// Create a new user
+router.post('/signup', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { username: req.body.username } });
+
+    if (!user) {
+      res.status(400).json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validPassword = await user.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+
+      res.json({ user: user, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
